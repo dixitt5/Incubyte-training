550b2763c79c7e1899f7775c1fa4e9ce89bc5b30
dixitt5 <tilajidixit@gmail.com>
2024-01-22 18:52:20 +0530

    add examples of generics and functions in ts

--- ts/generics_tuple.js
+++ ts/generics_tuple.js
@@ -1,31 +1,19 @@
-var Tuple = /** @class */ (function () {
-    function Tuple(_left, _center, _right) {
+"use strict";
+class Tuple {
+    constructor(_left, _center, _right) {
         this._left = _left;
         this._center = _center;
         this._right = _right;
     }
-    Object.defineProperty(Tuple.prototype, "left", {
-        get: function () {
-            return this._left;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Tuple.prototype, "center", {
-        get: function () {
-            return this._center;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Tuple.prototype, "right", {
-        get: function () {
-            return this._right;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Tuple;
-}());
-var d1 = new Tuple(10, true, "Hello");
-console.log("".concat(d1.left, " ").concat(d1.center, " ").concat(d1.right));
+    get left() {
+        return this._left;
+    }
+    get center() {
+        return this._center;
+    }
+    get right() {
+        return this._right;
+    }
+}
+const d1 = new Tuple(10, true, "Hello");
+console.log(`${d1.left} ${d1.center} ${d1.right}`);
