a3478c8135f82e29c8f679d08991926318cf72fb
Yash-Koshti <yashkoshti27@gmail.com>
2024-01-23 15:25:14 +0530

    Added two buttons

--- react-test/src/pages/Home.tsx
+++ react-test/src/pages/Home.tsx
@@ -2,19 +2,55 @@ import React from "react";
 import { PokeCard } from "../components/PokeCard";
 import { IPokemon, PokeType } from "../models/Pokemon";
 
+const [index, setIndex] = useState<number>(0);
+
+const getNextPokemon = () => {
+  useState(index + 1);
+}
+const getPreviousPokemon = () => {
+  useState(index - 1);
+}
 
 function Home() {
-  const pokemon : IPokemon = {
-    name: "charizard",
-    height: 123,
-    weight: 232,
-    type: PokeType.fire,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    id : 12
-  }
-  return (
-     <PokeCard pokemonObj={pokemon} />
-  );
+  const pokemon: IPokemon[] = [
+    {
+      name: "charizard",
+      height: 123,
+      weight: 232,
+      type: PokeType.fire,
+      imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      id: 12
+    },
+    {
+      name: "charizard",
+      height: 123,
+      weight: 232,
+      type: PokeType.fire,
+      imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+      id: 13
+    },
+    {
+      name: "charizard",
+      height: 123,
+      weight: 232,
+      type: PokeType.fire,
+      imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+      id: 14
+    },
+    {
+      name: "charizard",
+      height: 123,
+      weight: 232,
+      type: PokeType.fire,
+      imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      id: 15
+    }]
+  return <>(
+    <PokeCard pokemonObj={pokemon} />
+    <button onclick="getPreviousPokemon()">Prev</button>
+    )</>;
 }
 
 export default Home;
+
+
