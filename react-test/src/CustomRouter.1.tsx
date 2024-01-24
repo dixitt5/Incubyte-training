a4dc5bbee789ca1a7ea72243643de7ec35f2cf77
dixitt5 <tilajidixit@gmail.com>
2024-01-24 13:59:37 +0530

    webstorm setup

--- react-test/src/CustomRouter.1.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import Home from "./pages/Home";
-
-export const CustomRouter = () => {
-  const [route, setRoute] = useState<string>("home");
-  return <div>{route === "home" ? <Home /> : <Details />}</div>;
-};
