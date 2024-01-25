a0da1e4c296626e4d562c43f61207237165d3821
aaditya25052002 <adityaatiwariii@gmail.com>
2024-01-25 15:22:15 +0530

    dark moded

--- react-context/src/pages/Style.tsx
+++ react-context/src/pages/Style.tsx
@@ -1,14 +1,19 @@
+import {useState} from "react";
 
 export const Style = () => {
-    return (
-        <div>
-            <div className="container my-5 p-5 border rounded-3 bg-light text-dark" id="minimalistDiv" data-bs-theme="light">
-                <h2 className="mb-3">Dark Mode</h2>
-                <p>This is a simple div with a minimalist design using Bootstrap. Toggle the color mode to switch
-                    themes.</p>
-                <button className="btn btn-outline-secondary" onClick="toggleColorMode()">Toggle Color Mode</button>
-            </div>
+    const [darkMode, setDarkMode] = useState<boolean>(false);
 
+    // Toggle function to switch between light and dark mode
+    const toggleColorMode = () => {
+        setDarkMode(!darkMode);
+    };
+    const containerClass = `container my-5 p-5 border rounded-3 ${darkMode ? 'bg-dark text-white' : 'bg-light text-dark'}`;
+    return (
+        <div className={containerClass}>
+            <h2 className="mb-3">Minimalist Design</h2>
+            <p>This is a simple div with a minimalist design using Bootstrap. Toggle the color mode to switch
+                themes.</p>
+            <button className="btn btn-outline-secondary" onClick={toggleColorMode}>Toggle Color Mode</button>
         </div>
     );
 };
