3249643bcdc1b8edb292878abc07319e7fb7151f
dixitt5 <tilajidixit@gmail.com>
2024-01-27 15:17:24 +0530

    add a onclick function for routing to home page by clicking navbar title

--- react-test/src/components/Navbar.tsx
+++ react-test/src/components/Navbar.tsx
@@ -1,10 +1,19 @@
 import { type ReactElement } from 'react'
 import DarkModeToggle from './DarkModeToggle'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = ({ flag }: { flag: boolean }): ReactElement => {
+  const navigate = useNavigate()
+  const handleNavigation = (): void => {
+    try {
+      navigate('/pokemons')
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <div className="flex flex-row justify-between font-semibold dark:bg-green-800 dark:text-white bg-blue-400 text-black p-4 items-center">
-      <h1 className="text-4xl">Pokemons {flag ? 'Page' : 'Details'}</h1>
+      <h1 onClick={handleNavigation} className="text-4xl cursor-pointer">Pokemons {flag ? 'Page' : 'Details'}</h1>
       <DarkModeToggle/>
       {/* <button className='btn btn-secondary' onClick={() => { setIsDarkMode(!isDarkMode) }}>Change</button> */}
     </div>
