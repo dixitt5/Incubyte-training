04e9389f5f63407abd856a63353b5d6920a7e970
Manish <manishtulsiani.08@gmail.com>
2024-01-28 22:26:33 +0530

    fix: UI improvement in pagination component

--- react-test-naviz/src/components/Pagination.tsx
+++ react-test-naviz/src/components/Pagination.tsx
@@ -26,7 +26,7 @@ const Pagination = (props: IPaginationProps): ReactElement => {
                     <button onClick={() => {
                       setCurrentPage(i)
                     }}
-                            className={'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-green-500 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' + (currentPage === i ? ' bg-green-500 text-white' : '')}> {i}
+                            className={'flex items-center justify-center px-2 h-7 sm:px-3 sm:h-8 leading-tight text-gray-500 border border-gray-300 hover:bg-green-500 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white' + (currentPage === i ? ' bg-green-500 text-white' : '')}> {i}
                     </button>
                 </li>
       )
@@ -51,11 +51,11 @@ const Pagination = (props: IPaginationProps): ReactElement => {
 
   return (
         <div className="m-3 text-center">
-            <ul className="inline-flex -space-x-px text-sm">
+            <ul className="inline-flex -space-x-px text-xs sm:text-sm">
                 <li>
                     <button onClick={handlePrevious}
                             disabled={isFirstPage}
-                            className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            className="flex items-center justify-center px-2 h-7 sm:px-3 sm:h-8 ms-0  leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         Previous
                     </button>
                 </li>
@@ -63,7 +63,7 @@ const Pagination = (props: IPaginationProps): ReactElement => {
                 <li>
                     <button onClick={handleNext}
                             disabled={isLastPage}
-                            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            className="flex items-center justify-center px-2 h-7 sm:px-3 sm:h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         Next
                     </button>
                 </li>
