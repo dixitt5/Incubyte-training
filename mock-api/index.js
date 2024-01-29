6a3a48e073b18a8e703f0556692441dca5d0e01a
naveeez10 <naveezkhoja1063@gmail.com>
2024-01-29 18:21:01 +0530

    added java

--- mock-api/index.js
+++ mock-api/index.js
@@ -1,12 +1,13 @@
-import {faker } from '@faker-js/faker'
-import Express  from 'express';
+import {faker} from '@faker-js/faker'
+import Express from 'express';
 import lodash from 'lodash'
 
 const app = Express();
 const _ = lodash
 
 app.get('/users', (req, res) => {
-    res.send( _.times(5 , () => {
+    const query = req.query
+    res.send(_.times(query['id'], () => {
         return {
             userId: faker.string.uuid(),
             username: faker.internet.userName(),
@@ -15,25 +16,10 @@ app.get('/users', (req, res) => {
             password: faker.internet.password(),
             birthdate: faker.date.birthdate(),
             registeredAt: faker.date.past(),
-          }
+        }
     }))
 })
 
-app.get('/users/:id', (req, res) => {
-    const {id} = req.params
-    res.send( _.times(id , () => {
-        return {
-            userId: faker.string.uuid(),
-            username: faker.internet.userName(),
-            email: faker.internet.email(),
-            avatar: faker.image.avatar(),
-            password: faker.internet.password(),
-            birthdate: faker.date.birthdate(),
-            registeredAt: faker.date.past(),
-          }
-    }))
-})
-
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
