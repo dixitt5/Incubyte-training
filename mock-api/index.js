0d67b213276aa3d4f92a883d1f0d80ee8e49c4a3
dixitt5 <tilajidixit@gmail.com>
2024-01-29 16:32:02 +0530

    add json-server request route

--- mock-api/index.js
+++ mock-api/index.js
@@ -1,39 +1,37 @@
-import {faker } from '@faker-js/faker'
-import Express  from 'express';
+import {faker, fakerEN_IN } from '@faker-js/faker'
+import Express, { request }  from 'express';
 import lodash from 'lodash'
+import axios from 'axios';
+
+import  jsonServer from 'json-server';
 
 const app = Express();
 const _ = lodash
 
 app.get('/users', (req, res) => {
-    res.send( _.times(5 , () => {
+    const {count} = req.query
+    res.send( _.times(count , () => {
         return {
-            userId: faker.string.uuid(),
-            username: faker.internet.userName(),
-            email: faker.internet.email(),
-            avatar: faker.image.avatar(),
-            password: faker.internet.password(),
-            birthdate: faker.date.birthdate(),
-            registeredAt: faker.date.past(),
+            userId: fakerEN_IN.string.uuid(),
+            username: fakerEN_IN.internet.userName(),
+            email: fakerEN_IN.internet.email(),
+            avatar: fakerEN_IN.image.avatar(),
+            password: fakerEN_IN.internet.password(),
+            birthdate: fakerEN_IN.date.birthdate(),
+            registeredAt: fakerEN_IN.date.past(),
+            state : fakerEN_IN.location.state(),
           }
     }))
 })
 
-app.get('/users/:id', (req, res) => {
-    const {id} = req.params
-    res.send( _.times(id , () => {
-        return {
-            userId: faker.string.uuid(),
-            username: faker.internet.userName(),
-            email: faker.internet.email(),
-            avatar: faker.image.avatar(),
-            password: faker.internet.password(),
-            birthdate: faker.date.birthdate(),
-            registeredAt: faker.date.past(),
-          }
-    }))
+app.use("api/", jsonServer.router("db.json"));
+
+app.get("/pokemons/results", async (req, res) => {
+  const result = await axios.get("http://localhost:3000/results");
+  console.log(result.data)
+  res.json(result.data)
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+app.listen(3001, ()=>{
+    console.log('Server is running on port 3001');
 });
