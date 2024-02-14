b0eaadb40c4366e9d0d46347e21f0a2973b137f4
aaditya25052002 <--unset>
2024-02-14 16:44:34 +0530

    add: products.service

--- nest-tutorial/src/products/products.controller.spec.ts
+++ nest-tutorial/src/products/products.controller.spec.ts
@@ -2,13 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductRequestDTO } from './productRequestDTO';
 import { ProductResponseDTO } from './productResponseDTO';
+import { mockDeep } from 'jest-mock-extended';
+import { ProductsService } from './products.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  const service = mockDeep<ProductsService>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
+      providers: [{ provide: ProductsService, useValue: service }],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
@@ -19,10 +23,12 @@ describe('ProductsController', () => {
   });
 
   it('should return empty array', () => {
+    service.getProducts.mockReturnValue([]);
     expect(controller.getProducts()).toMatchObject([]);
   });
 
   it('should add a new product', () => {
+    service.addProduct.mockReturnValue({ id: 1, name: 'test', price: 10 });
     //complete this
     const expectedProduct: ProductResponseDTO = {
       id: 1,
@@ -32,15 +38,28 @@ describe('ProductsController', () => {
     expect(
       controller.addProduct({ name: 'test', price: 10 } as ProductRequestDTO),
     ).toMatchObject(expectedProduct);
+
+    expect(service.addProduct).toHaveBeenCalledWith({
+      name: 'test',
+      price: 10,
+    });
   });
 
   it('should return list of products', () => {
+    // arrange
+    service.getProducts.mockReturnValue([{ id: 1, name: 'test', price: 10 }]);
     const expectedProduct: ProductResponseDTO = {
       id: 1,
       name: 'test',
       price: 10,
     };
     controller.addProduct({ name: 'test', price: 10 } as ProductRequestDTO);
-    expect(controller.getProducts()).toMatchObject([expectedProduct]);
+
+    // act
+    const products = controller.getProducts();
+
+    // assert
+    expect(service.getProducts).toHaveBeenCalled();
+    expect(products).toMatchObject([expectedProduct]);
   });
 });
--- nest-tutorial/src/products/products.controller.ts
+++ nest-tutorial/src/products/products.controller.ts
@@ -1,19 +1,25 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductRequestDTO } from './productRequestDTO';
 import { ProductResponseDTO } from './productResponseDTO';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   private products: ProductResponseDTO[] = [];
+  private service: ProductsService;
+
+  constructor(service: ProductsService) {
+    this.service = service;
+  }
   @Get()
   getProducts() {
-    return this.products;
+    return this.service.getProducts();
   }
 
   @Post()
   addProduct(@Body() newProduct: ProductRequestDTO) {
-    const addedProduct: ProductResponseDTO = { id: 1, ...newProduct };
-    this.products.push(addedProduct);
+    const addedProduct: ProductResponseDTO =
+      this.service.addProduct(newProduct);
     return addedProduct;
   }
 }
