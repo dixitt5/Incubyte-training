e0246192b45c14b699e5f8f7225808ae3d946c7b
aaditya25052002 <--unset>
2024-02-15 14:28:15 +0530

    add: Order's post

--- nest-tutorial/src/app.module.ts
+++ nest-tutorial/src/app.module.ts
@@ -5,10 +5,12 @@ import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { OrderController } from './order/order.controller';
+import { OrderService } from './order/order.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService, PrismaService],
+  controllers: [AppController, ProductsController, OrderController],
+  providers: [AppService, ProductsService, PrismaService, OrderService],
 })
 export class AppModule {}
